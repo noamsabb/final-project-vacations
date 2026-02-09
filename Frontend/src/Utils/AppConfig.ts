b2341585b3beb10138510bb1f2951769c100b58e
noamsabb <sabban.noam@gmail.com>
2026-02-09 17:15:17 +0200

    deployment fixs

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,22 +1,16 @@
-class LocalDevelopmentConfig {
-	public readonly vacationsUrl = "http://localhost:4000/api/vacations/"; 
-	public readonly registerUrl = "http://localhost:4000/api/register/"; 
-	public readonly loginUrl = "http://localhost:4000/api/login/"; 
-	public readonly userUrl = "http://localhost:4000/api/user/"; 
+class AppConfig {
+  // Check if we're in development mode
+  private readonly isDevelopment = import.meta.env.DEV;
+  
+  // Use localhost in development, production URL when deployed
+  private readonly baseUrl = this.isDevelopment
+    ? "http://localhost:4000"
+    : "https://vacation-backend-8yfa.onrender.com";
+  
+  public readonly vacationsUrl = `${this.baseUrl}/api/vacations/`;
+  public readonly registerUrl = `${this.baseUrl}/api/register/`;
+  public readonly loginUrl = `${this.baseUrl}/api/login/`;
+  public readonly userUrl = `${this.baseUrl}/api/user/`;
 }
-// class DockerDevelopmentConfig {
-// 	public readonly vacationsUrl = "http://localhost:40001/api/vacations/"; 
-// 	public readonly registerUrl = "http://localhost:4001/api/register/"; 
-// 	public readonly loginUrl = "http://localhost:40001/api/login/"; 
-// 	public readonly userUrl = "http://localhost:40001/api/user/"; 
-// }
-// class ProductionConfig {
-// 	public readonly vacationsUrl = "http://44.244.140.96:4001/api/vacations/"; 
-// 	public readonly registerUrl = "http://44.244.140.96:4001/api/register/"; 
-// 	public readonly loginUrl = "http://44.244.140.96:4001/api/login/"; 
-// 	public readonly userUrl = "http://44.244.140.96:4001/api/user/"; 
-// }
 
-// export const appConfig = new DockerDevelopmentConfig();
-export const appConfig = new LocalDevelopmentConfig();
-// export const appConfig = new ProductionConfig();
+export const appConfig = new AppConfig();
